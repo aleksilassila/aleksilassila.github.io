ef4f485e099ca8cfa3bc9a4a84619e1f80bfb87c
Aleksi Lassila <aleksi.emil.lassila@gmail.com>
2020-08-26 21:07:50 +0300

    Updated color once more

--- frontend/src/theme/theme.js
+++ frontend/src/theme/theme.js
@@ -9,12 +9,13 @@ const theme = {
         purple: "#971cd2",
     },
 
-    // text: "#231e3a",
-    text: "#d7d2ef",
-    separators: "#00000033",
+    text: "#231e3a",
+    // text: "#d7d2ef",
+    separators: "#00000033", // Will be set based on text color later
     backgroundGradient:
-        "linear-gradient(20deg, rgb(158 59 165), rgb(88 126 197))",
-    invertedIcons: true,
+        // "linear-gradient(20deg, rgb(158 59 165), rgb(88 126 197))",
+        "linear-gradient(20deg, hsl(296 47% 57% / 1) 0%, hsl(219 48% 50% / 1) 80%)",
+    invertedIcons: false,
 };
 
 theme.separators = `${theme.text}33`;
